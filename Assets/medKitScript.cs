30114fefc859fc61502dde49615622379e942caa
yydjay1100 <yangyidong@yangyidongdeMacBook-Pro.local>
2017-03-01 12:16:17 -0800

    3.1
    add medkit, but some problem cannot be solved.

---
+++ Assets/medKitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class medKitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine ("Move");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector3.forward * 3f * Time.deltaTime);
+	}
+
+}
